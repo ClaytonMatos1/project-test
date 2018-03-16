845b5f4c7d2e376e7329884abe4383e3d2e53fcb
cmatos <cmatos@uolinc.com>
2018-03-16 18:41:26 -0300

    [master] Alteracoes de pacotes: Alteracoes no arquivo do gulp

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -2,68 +2,66 @@
 
 import gulp         from 'gulp';
 import clean        from 'gulp-clean';
-import concat       from 'gulp-concat';
 import sourcemaps   from 'gulp-sourcemaps';
 import browserify   from 'browserify';
-import babelify     from 'babelify';
-import vueify       from 'vueify';
 import fs           from 'fs';
 import shell        from 'gulp-shell';
 import uglify       from 'gulp-uglify';
 import browserSync  from 'browser-sync';
 
 const paths = {
-    src  : 'src',
-    dist : 'dist'
+    src  : 'src/',
+    dist : 'dist/'
 };
+const fileDest = 'build.js';
 
 gulp.task('default', ['map']);
 
-gulp.task('clean', () => gulp.src(paths.dist).pipe(clean()));
-
-gulp.task('dist', ['clean'], () => {
-    return gulp.src('./')
-        .pipe(shell('mkdir ' + paths.dist))
-        .pipe(shell('cd dist && touch build.js'))
-});
-
-gulp.task('build', ['dist'], () => {
-    return browserify({
-            entries: [paths.src + '/main.js'],
-            transform : ['vueify', 'babelify']
-        })
-        .bundle()
-        .pipe(fs.createWriteStream(paths.dist + '/build.js'));
-});
-
-gulp.task('uglify', ['build'], () => {
-    return gulp.src(paths.dist + '/build.js')
-        .pipe(uglify())
-        .pipe(gulp.dest(paths.dist));
-})
-
 gulp.task('map', ['uglify'], () => {
-    return gulp.src(paths.dist + '/build.js')
+    return gulp.src(paths.dist + fileDest)
         .pipe(sourcemaps.init())
         .pipe(sourcemaps.write())
         .pipe(gulp.dest(paths.dist));
 });
 
-gulp.task('server', ['uglify'], () => {
+gulp.task('uglify', ['build'], () => {
+    return gulp.src(paths.dist + fileDest)
+        .pipe(uglify())
+        .pipe(gulp.dest(paths.dist));
+});
+
+gulp.task('build', ['dist'], () => {
+    return browserify({
+            entries: [paths.src + 'main.js'],
+            transform : ['vueify', 'babelify']
+        })
+        .bundle()
+        .pipe(fs.createWriteStream(paths.dist + fileDest));
+});
+
+gulp.task('dist', ['clean'], () => {
+    return gulp.src('./')
+        .pipe(shell('mkdir dist'))
+        .pipe(shell('cd dist && touch ' + fileDest));
+});
+
+gulp.task('clean', () => gulp.src(paths.dist).pipe(clean()));
+
+gulp.task('server', ['map'], () => {
     browserSync.init({
         server : {
             baseDir : './'
-        }
+        },
+        reloadDelay: 500,
     });
-    gulp.watch('src/**/*').on('change', (event) => {
-        return browserify({
-                entries: [paths.src + '/main.js'],
-                transform : ['vueify', 'babelify']
-            })
-            .bundle()
-            .pipe(fs.createWriteStream(paths.dist + '/build.js'));
-    });
-    gulp.watch('src/**/*').on('change', (event) => {
-        setTimeout(() => browserSync.reload, 1000);
-    });
+    gulp.watch('src/**/*', ['refresh']).on('change', browserSync.reload);
+});
+
+gulp.task('refresh', () => {
+    return browserify({
+            entries: [paths.src + 'main.js'],
+            transform : ['vueify', 'babelify']
+        })
+        .bundle()
+        .pipe(fs.createWriteStream(paths.dist + fileDest));
 });
